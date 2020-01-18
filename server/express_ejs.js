3f6563fdbc6da1d9f174440a6e4bacbdf392fec0
KH <kh@kh>
2020-01-18 14:23:03 -0600

    structure 3

--- server/express_ejs.js
+++ server/express_ejs.js
@@ -2,4 +2,5 @@ exports.setup = function (app) {
     app.get('/', function(req, res) {
         res.render(__dirname + '/views/index.ejs');
     });
+    app.use('/', require('./public_controller/user_generic'));
 };
