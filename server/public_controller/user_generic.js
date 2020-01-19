ccb9d80b8944b9eea381a059f45c473dadccf8e4
e85b9457-6ceb-411a-8080-81b6c2fcdde7 <datnguyen5653@huskers.unl.edu>
2020-01-18 21:05:40 -0600

    code

--- server/public_controller/user_generic.js
+++ server/public_controller/user_generic.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const ReqWrapper = require('../wrapper/req');
 const router = module.exports = express.Router();
+const ash = require('express-async-handler');
 
+const CANVAS_URL = "https://canvas.instructure.com/";
 router.get('/', function(req, res) {
     res.render(__dirname + '/views/index.ejs');
 });
@@ -17,13 +20,41 @@ router.get('/pleaselogin', (req, res) => {
   return res.render(__dirname + '/views/pleaselogin.ejs');
 });
 
-router.get('/courses/:id', (req, res) => {
+router.get('/courses/:id', ash(async(req, res, next) => {
   const sess = req.session;
-  console.log('sess', sess);
-  return res.render(__dirname + '/views/course.ejs');
-});
+  const { id } = req.params;
+  const rw = new ReqWrapper(req, res, next);
+  
+  console.log('req.params', req.params, 'id', id);
+  if (!sess.canvas) {
+    return res.redirect(CANVAS_URL);
+  }
+  
+  let course = null; 
+  try {
+    course = await rw.get_course_status(id);
+  } catch (e) {
+    console.log('error', e);
+    return res.redirect(CANVAS_URL);
+  }
+
+  console.log('course', course);
+  return res.render(__dirname + '/views/course.ejs', {
+    sess: sess,
+    course: course,
+  });
+}));
 
 router.get('/dashboard', (req, res) => {
-    return res.render(__dirname + '/views/dashboard.ejs');
+  const sess = req.session;
+  //if sess null?
+  
+  if (!sess.canvas) {
+    return res.redirect(CANVAS_URL);
+  }
+
+  return res.render(__dirname + '/views/dashboard.ejs', {
+    sess: sess
+  });
 });
 
